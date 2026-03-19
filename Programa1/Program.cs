cc2f4ff06f25f9579f512877d700e85eaa8c0615
ALUMNO <caxajgabriel2@gmail.com>
2026-03-19 11:13:07 -0600

    Agregar archivos de proyecto.

---
+++ Programa1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Ingrese su edad: ");
+int edad;
+
+
+if (int.TryParse(Console.ReadLine(), out edad))
+{
+    Console.WriteLine("No es valido");
+}
+
+
+Console.ReadKey();
+Console.WriteLine("Validad edad: ");
+
+if (edad >= 1 && edad < 120)
+{
+    Console.WriteLine("Edad valida");
+}
+else
+{
+    Console.WriteLine("Edad no valida");
+}
+
