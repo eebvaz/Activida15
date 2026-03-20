705d475e172294f462306fc45f52a4fed7f459d7
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-19 18:43:41 -0600

    Tercera lista

--- Programa3/Program.cs
+++ Programa3/Program.cs
@@ -1,5 +1,24 @@
 ﻿
 
+int cantidad;
+bool correcto=false;
 
+while (!correcto)
+{
+   
+    Console.Write("Ingrese la cantidad de estudiantes: ");
 
-Console.WriteLine("Es valido el precio: ");
+    if (int.TryParse(Console.ReadLine(), out cantidad) && cantidad >0)
+      {
+
+        correcto = true;
+    }
+    else
+    {
+        Console.WriteLine("Numero erroneo vuelve a ingresar");
+        Console.ReadKey();
+    }
+    Console.Clear();
+}
+
+Console.WriteLine("Se ha validado correctamente");
