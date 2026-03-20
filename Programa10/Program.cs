ba8b458da78b13e9088aa6f2fe9d067e4dd3cd0c
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-19 19:49:52 -0600

    Decimo listo

--- Programa10/Program.cs
+++ Programa10/Program.cs
@@ -1,1 +1,26 @@
 ﻿
+int cantidad, producto;
+bool correcto = false;
+string productos = "";
+
+while (!correcto)
+{
+    Console.Write("Ingrese una cantidad de productos: ");
+
+    if (int.TryParse(Console.ReadLine(), out cantidad) && cantidad > 0)
+    {
+        Console.WriteLine("");
+        correcto = true;
+    }
+    else
+    {
+
+        Console.WriteLine("Cantidad incorrecta");
+        Console.ReadKey();
+    }
+    Console.Clear();
+}
+
+Console.WriteLine("Ingrese el producto");
+productos = Console.ReadLine();
+
