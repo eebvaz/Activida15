fb20770ce68bcaccddbaf6ec818aa17363755111
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-19 19:31:09 -0600

    Septimo listo

--- Programa7/Program.cs
+++ Programa7/Program.cs
@@ -1,1 +1,23 @@
 ﻿
+
+double salario=0;
+
+bool correcto = false;
+
+while (!correcto)
+{
+    Console.Write("Ingrese Su Salario: ");
+
+    if (double.TryParse(Console.ReadLine(), out salario) && salario >=0)
+    {
+        Console.WriteLine("");
+        correcto = true;
+    }
+    else
+    {
+        Console.WriteLine("Numero invalido");
+    }
+    Console.Clear();
+}
+
+Console.WriteLine("Salario Correcto: Q"+salario);
