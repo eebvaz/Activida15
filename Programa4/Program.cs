6ddd9d5e773fc6a2e7053b81243080946c024e53
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-19 18:57:40 -0600

    Cuarta lista

--- Programa4/Program.cs
+++ Programa4/Program.cs
@@ -1,1 +1,25 @@
 ﻿
+Console.WriteLine("");
+
+int fecha;
+
+bool correcto= false;
+
+
+while (!correcto)
+{
+    Console.WriteLine("Ingrese su fecha de nacimiento; ");
+
+    if (int.TryParse(Console.ReadLine(), out fecha) && fecha<=2026 && fecha >1900)
+    {
+        Console.WriteLine("");
+        correcto=true;
+    }
+    else
+    {
+        Console.WriteLine("Fecha invalida");
+    }
+
+}
+
+Console.WriteLine("Fecha valida");
