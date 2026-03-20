1a58b12c6d1855e7bf9fcb9415bd2cd62ba9fe5a
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-19 19:20:08 -0600

    Noveno listo

--- Programa9/Program.cs
+++ Programa9/Program.cs
@@ -1,1 +1,32 @@
 ﻿
+
+int nota=0;
+bool correcto = false;
+
+while (!correcto)
+{
+    Console.Write("Ingrese la nota: ");
+
+    if (int.TryParse(Console.ReadLine(), out nota) && nota >= 1 && nota <=100)
+    {
+        Console.WriteLine("");
+        correcto = true;
+    }
+    else
+    {
+
+        Console.WriteLine("Nota invalida");
+    }
+    Console.Clear();
+}
+
+
+ if(nota >=61)
+{
+    Console.WriteLine("Aprovado");
+}
+ else
+{
+    Console.WriteLine("Reproavado");
+}
+
