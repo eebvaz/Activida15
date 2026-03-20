51a6850dd7aaa09b26859aa14ed75cab8b15eead
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-19 19:45:54 -0600

    Sexto listo

--- Programa6/Program.cs
+++ Programa6/Program.cs
@@ -1,2 +1,23 @@
 ﻿
 
+DateTime fecha;
+
+bool correcto = false;
+
+while (!correcto)
+{
+    Console.Write("Ingrese una fecha con el formato (dia/mes/año): ");
+
+    if (DateTime.TryParse(Console.ReadLine(), out fecha))
+    {
+        correcto = true;
+    }
+    else
+    {
+
+        Console.WriteLine("Fecha incorrecto");
+    }
+    Console.Clear();
+}
+
+Console.WriteLine($"Fecha validada correctamente");
