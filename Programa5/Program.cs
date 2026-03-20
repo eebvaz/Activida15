7f8c2c07c185475fd99766fefc6bf2ac0d0b286f
eebvaz <ee.bvasquez@udeo.edu.gt>
2026-03-19 19:08:48 -0600

    Quinto listo

--- Programa5/Program.cs
+++ Programa5/Program.cs
@@ -1,1 +1,36 @@
 ﻿
+
+
+int numero=0;
+bool correcto = false;  
+
+
+while  (!correcto)
+{
+    Console.Write("Ingrese un numero: ");
+
+   if (int.TryParse(Console.ReadLine(), out numero))
+    {
+
+        correcto = true;
+
+    }
+   else
+    {
+        Console.Write("No es numero entero");
+    }
+
+}
+
+Console.WriteLine("Es numero entero");
+
+if (numero % 2 == 0)
+{
+    Console.Write("Es par ");
+}
+else
+{
+    Console.Write("Es impar");
+}
+
+Console.ReadKey();
